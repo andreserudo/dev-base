f874736ade91fb38ff26bb03586cfb0e4c06f058
andreserudo <andreserudo@gmail.com>
2021-06-25 11:50:29 -0400

    first commit

---
+++ backend/index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+
+const PORT = process.env.PORT || 3001;
+
+const app = express();
+
+app.use(cors());
+
+app.use(express.json());
+
+app.listen(PORT, () => console.log(`No ar pela ${PORT}`));
